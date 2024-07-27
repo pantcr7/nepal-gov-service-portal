a06dfc7a3ba95e3be7ab41c4904961a73ec799bf
Prasant <prasantpant141@gmail.com>
2024-07-28 00:31:05 +0545

    first commit

---
+++ nepal_the_service/api.py
@@ -0,0 +1,21 @@
+import frappe
+from frappe import _
+
+@frappe.whitelist()
+def get_user_info():
+
+    if frappe.request.method != "GET":
+        frappe.throw(_("Method Not Allowed"), frappe.PermissionError)
+    try:
+        if not frappe.session.user:
+            frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+
+        user = frappe.get_doc("User", frappe.session.user, ignore_permissions=True)
+        return {
+            "user_info": {
+                "name": user.first_name,
+                "user_image": user.user_image,
+            }
+        }
+    except Exception as e:
+        frappe.throw(str(e))
